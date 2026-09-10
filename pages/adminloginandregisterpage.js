cae0c5bdad0afaff3a382f25694488b8a7fa93e5
Sanat Singh <sanat.singh.2021@uni.strath.ac.uk>
2026-09-10 21:16:15 +0100

    Updated Formating

--- pages/adminloginandregisterpage.js
+++ pages/adminloginandregisterpage.js
@@ -1,50 +1,50 @@
 const { expect } = require('@playwright/test');
 
-class AdminLoginAndRegisterPage
-{
-    constructor(page)
-    {
+class AdminLoginAndRegisterPage {
+    constructor(page) {
         this.page = page;
     }
-    async adminlogin(username, password)
-    {
-    await this.page.fill('input[type="email"]', username);
-    await this.page.fill('input[type="password"]', password);
-    await this.page.waitForTimeout(3000); // Wait for 2 seconds before clicking the login button
-    await this.page.click("//button[normalize-space()='Log In']");
-    await this.page.waitForTimeout(3000); // Wait for 2 seconds before clicking the login button
 
-  }
+    async adminlogin(username, password) {
+        await this.page.fill('input[type="email"]', username);
+        await this.page.fill('input[type="password"]', password);
+        await this.page.waitForTimeout(3000); // Wait for 2 seconds before clicking the login button
+        await this.page.click("//button[normalize-space()='Log In']");
+        await this.page.waitForTimeout(3000); // Wait for 2 seconds before clicking the login button
 
-   async checkLoginSuccess() {
-          await expect(this.page).toHaveURL(/admin/);
-  }
+    }
 
-async addtoCart() {
- const [newPage] = await Promise.all([
-        this.page.waitForEvent('popup'),
-        this.page.click("//a[normalize-space()='View Store']")
-    ]);
+    async checkLoginSuccess() {
+        await expect(this.page).toHaveURL(/admin/);
+    }
 
-    await newPage.waitForLoadState();
-    const productLocator = newPage.locator(
-    "//a[normalize-space()='JB Fragrances and Flavours - GOLD BD']"
-);
-    await productLocator.waitFor({ state: 'visible' });
-    const productName = await productLocator.textContent();
- const productPrice = await productLocator
-        .locator("xpath=..")
-        .locator("span[class='text-lg font-bold text-ink']")
-        .textContent();
-    await newPage.click(
-    "//button[@class='btn-primary mt-2 w-full'][normalize-space()='Add to Cart'][1]"
-);
-await newPage.click("//span[@class='relative']//*[name()='svg']");
+    async addtoCart() {
+        const [newPage] = await Promise.all([
+            this.page.waitForEvent('popup'),
+            this.page.click("//a[normalize-space()='View Store']")
+        ]);
 
-    await newPage.waitForTimeout(2000);
-    return { productName, productPrice, newPage };
- }
+        await newPage.waitForLoadState();
+        const productLocator = newPage.locator(
+            "//a[normalize-space()='JB Fragrances and Flavours - GOLD BD']"
+        );
 
+        await productLocator.waitFor({ state: 'visible' });
+        const productName = await productLocator.textContent();
+        const productPrice = await productLocator
+            .locator("xpath=..")
+            .locator("span[class='text-lg font-bold text-ink']")
+            .textContent();
+
+        await newPage.click(
+            "//button[@class='btn-primary mt-2 w-full'][normalize-space()='Add to Cart'][1]"
+        );
+
+        await newPage.click("//span[@class='relative']//*[name()='svg']");
+
+        await newPage.waitForTimeout(2000);
+        return { productName, productPrice, newPage };
+    }
 
 }
 
--- tests/adminlogin.spec.js
+++ tests/adminlogin.spec.js
@@ -1,11 +1,11 @@
-const{test,expect}=require('@playwright/test')
-const Homepage=require('../pages/homepage')
-const AdminLoginAndRegister=require('../pages/adminloginandregisterpage')
+const { test, expect } = require('@playwright/test')
+const Homepage = require('../pages/homepage')
+const AdminLoginAndRegister = require('../pages/adminloginandregisterpage')
 
 const ADMIN_USERNAME = 'admin@agarbattikart.com'
 const ADMIN_PASSWORD = 'Admin@123'
 
-test("verify the login functionality with valid credentials",async({page}) =>{
+test("verify the login functionality with valid credentials", async ({ page }) => {
     const homepage = new Homepage(page)
     const adminLoginAndRegister = new AdminLoginAndRegister(page)
     await homepage.gotohomepage()
@@ -18,32 +18,31 @@ test("verify the login functionality with valid credentials",async({page}) =>{
 
 
 
-test("verify the same product is added to cart with the same price",async({page}) => {
+test("verify the same product is added to cart with the same price", async ({ page }) => {
     const homepage = new Homepage(page)
     const adminLoginAndRegister = new AdminLoginAndRegister(page)
     await homepage.gotohomepage()
     await homepage.goToLoginPage()
     await adminLoginAndRegister.adminlogin(ADMIN_USERNAME, ADMIN_PASSWORD)
-    const { productName, productPrice, newPage } =
-    await adminLoginAndRegister.addtoCart();
+    const { productName, productPrice, newPage } = await adminLoginAndRegister.addtoCart();
 
-   console.log("Selected product:", productName);
+    console.log("Selected product:", productName);
     console.log("Selected price:", productPrice);
- const cartProduct = newPage.locator(
-    "//a[normalize-space()='JB Fragrances and Flavours - GOLD BD']"
-);
 
+    const cartProduct = newPage.locator(
+        "//a[normalize-space()='JB Fragrances and Flavours - GOLD BD']"
+    );
 
-const cartProductName = await cartProduct.textContent();
+    const cartProductName = await cartProduct.textContent();
 
+    const cartProductPrice = await cartProduct
+        .locator("xpath=..")
+        .locator("p")
+        .textContent();
 
-const cartProductPrice = await cartProduct
-    .locator("xpath=..")
-    .locator("p")
-    .textContent();
-   console.log("Cart product:", cartProductName);
-console.log("Cart price:", cartProductPrice);
+    console.log("Cart product:", cartProductName);
+    console.log("Cart price:", cartProductPrice);
 
-expect(cartProductName).toBe(productName);
-expect(cartProductPrice).toContain(productPrice);
+    expect(cartProductName).toBe(productName);
+    expect(cartProductPrice).toContain(productPrice);
 })
